3df76f88bbb70cf727c68800f3e6a0169005b9e8
tom <tommaso@pavese.me>
2014-04-05 22:01:38 +0200

    updating variable names in test files

--- test/creation_test.rb
+++ test/creation_test.rb
@@ -5,123 +5,123 @@ class CreationTest < Minitest::Test
 
   def test_create_with_now
     @time = Time.now
-    @st_time = DatelessTime.now
+    @dl_time = DatelessTime.now
 
-    assert_equal @time.hour, @st_time.hours
-    assert_equal @time.min, @st_time.minutes
-    assert_equal @time.sec, @st_time.seconds
-    assert_equal @time.strftime("%H:%M:%S"), @st_time.to_s
-    assert_equal (@time.sec + (@time.min * 60) + (@time.hour * 60 * 60)), @st_time.to_i
+    assert_equal @time.hour, @dl_time.hours
+    assert_equal @time.min, @dl_time.minutes
+    assert_equal @time.sec, @dl_time.seconds
+    assert_equal @time.strftime("%H:%M:%S"), @dl_time.to_s
+    assert_equal (@time.sec + (@time.min * 60) + (@time.hour * 60 * 60)), @dl_time.to_i
   end
 
 
   def test_create_with_time
     @time = Time.new 2014, 1, 1, 13, 37, 42
-    @st_time = DatelessTime.new @time
+    @dl_time = DatelessTime.new @time
 
-    assert_equal 13, @st_time.hours
-    assert_equal 37, @st_time.minutes
-    assert_equal 42, @st_time.seconds
-    assert_equal (42 + (37 * 60) + (13 * 60 * 60)), @st_time.to_i
+    assert_equal 13, @dl_time.hours
+    assert_equal 37, @dl_time.minutes
+    assert_equal 42, @dl_time.seconds
+    assert_equal (42 + (37 * 60) + (13 * 60 * 60)), @dl_time.to_i
   end
 
 
 
   def test_create_with_string
-    @st_time = DatelessTime.new "12:13:14"
+    @dl_time = DatelessTime.new "12:13:14"
   
-    assert_equal 12, @st_time.hours
-    assert_equal 13, @st_time.minutes
-    assert_equal 14, @st_time.seconds
-    assert_equal (14 + (13 * 60) + (12 * 60 * 60)), @st_time.to_i
+    assert_equal 12, @dl_time.hours
+    assert_equal 13, @dl_time.minutes
+    assert_equal 14, @dl_time.seconds
+    assert_equal (14 + (13 * 60) + (12 * 60 * 60)), @dl_time.to_i
   end
 
 
 
   def test_create_with_string_without_seconds
-    @st_time = DatelessTime.new "12:13"
+    @dl_time = DatelessTime.new "12:13"
   
-    assert_equal 12, @st_time.hours
-    assert_equal 13, @st_time.minutes
-    assert_equal 0, @st_time.seconds
-    assert_equal ((13 * 60) + (12 * 60 * 60)), @st_time.to_i
+    assert_equal 12, @dl_time.hours
+    assert_equal 13, @dl_time.minutes
+    assert_equal 0, @dl_time.seconds
+    assert_equal ((13 * 60) + (12 * 60 * 60)), @dl_time.to_i
   end
 
 
   def test_create_with_string_without_seconds_and_minutes
-    @st_time = DatelessTime.new "12"
+    @dl_time = DatelessTime.new "12"
   
-    assert_equal 12, @st_time.hours
-    assert_equal 0, @st_time.minutes
-    assert_equal 0, @st_time.seconds
-    assert_equal (12 * 60 * 60), @st_time.to_i
+    assert_equal 12, @dl_time.hours
+    assert_equal 0, @dl_time.minutes
+    assert_equal 0, @dl_time.seconds
+    assert_equal (12 * 60 * 60), @dl_time.to_i
   end
 
 
 
   def test_create_with_bad_string
     assert_raises DatelessTime::InitializationError do
-      @st_time = DatelessTime.new "hello"
+      @dl_time = DatelessTime.new "hello"
     end
   end
 
 
   def test_create_with_bad_numerical_string
     assert_raises DatelessTime::InitializationError do
-      @st_time = DatelessTime.new "1000"
+      @dl_time = DatelessTime.new "1000"
     end
   end
 
 
   def test_create_with_empty_string
     assert_raises DatelessTime::InitializationError do
-      @st_time = DatelessTime.new("")
+      @dl_time = DatelessTime.new("")
     end
   end
 
 
   def test_create_with_string_with_am
-    @st_time = DatelessTime.new "10:13:14 am"
+    @dl_time = DatelessTime.new "10:13:14 am"
   
-    assert_equal 10, @st_time.hours
-    assert_equal 13, @st_time.minutes
-    assert_equal 14, @st_time.seconds
-    assert_equal (14 + (13 * 60) + (10 * 60 * 60)), @st_time.to_i
+    assert_equal 10, @dl_time.hours
+    assert_equal 13, @dl_time.minutes
+    assert_equal 14, @dl_time.seconds
+    assert_equal (14 + (13 * 60) + (10 * 60 * 60)), @dl_time.to_i
 
-    @st_time = DatelessTime.new "10:13AM"
+    @dl_time = DatelessTime.new "10:13AM"
   
-    assert_equal 10, @st_time.hours
-    assert_equal 13, @st_time.minutes
-    assert_equal 0, @st_time.seconds
-    assert_equal ((13 * 60) + (10 * 60 * 60)), @st_time.to_i
+    assert_equal 10, @dl_time.hours
+    assert_equal 13, @dl_time.minutes
+    assert_equal 0, @dl_time.seconds
+    assert_equal ((13 * 60) + (10 * 60 * 60)), @dl_time.to_i
   end
 
 
   def test_create_with_string_with_pm
-    @st_time = DatelessTime.new "10:13:14 pm"
+    @dl_time = DatelessTime.new "10:13:14 pm"
   
-    assert_equal 22, @st_time.hours
-    assert_equal 13, @st_time.minutes
-    assert_equal 14, @st_time.seconds
-    assert_equal (14 + (13 * 60) + (22 * 60 * 60)), @st_time.to_i
+    assert_equal 22, @dl_time.hours
+    assert_equal 13, @dl_time.minutes
+    assert_equal 14, @dl_time.seconds
+    assert_equal (14 + (13 * 60) + (22 * 60 * 60)), @dl_time.to_i
 
-    @st_time = DatelessTime.new "10:13PM"
+    @dl_time = DatelessTime.new "10:13PM"
   
-    assert_equal 22, @st_time.hours
-    assert_equal 13, @st_time.minutes
-    assert_equal 0, @st_time.seconds
-    assert_equal ((13 * 60) + (22 * 60 * 60)), @st_time.to_i
+    assert_equal 22, @dl_time.hours
+    assert_equal 13, @dl_time.minutes
+    assert_equal 0, @dl_time.seconds
+    assert_equal ((13 * 60) + (22 * 60 * 60)), @dl_time.to_i
   end
 
 
 
   def test_create_with_string_with_nonsensical_pm
     assert_raises DatelessTime::TimeOutOfRangeError do
-      @st_time = DatelessTime.new "15:13:14 pm"
+      @dl_time = DatelessTime.new "15:13:14 pm"
     end
   
     assert_raises DatelessTime::TimeOutOfRangeError do
-      @st_time = DatelessTime.new "13:13PM"
+      @dl_time = DatelessTime.new "13:13PM"
     end
   end
 
@@ -129,163 +129,163 @@ class CreationTest < Minitest::Test
 
   def test_create_with_seconds
     secs = 59 + (35 * 60) + (8 * 60 * 60)
-    @st_time = DatelessTime.new secs
+    @dl_time = DatelessTime.new secs
 
-    assert_equal 8, @st_time.hours
-    assert_equal 35, @st_time.minutes
-    assert_equal 59, @st_time.seconds
-    assert_equal secs, @st_time.to_i
+    assert_equal 8, @dl_time.hours
+    assert_equal 35, @dl_time.minutes
+    assert_equal 59, @dl_time.seconds
+    assert_equal secs, @dl_time.to_i
   end
 
 
   def test_create_with_very_few_seconds
     secs = 10 + (2 * 60)
-    @st_time = DatelessTime.new secs
+    @dl_time = DatelessTime.new secs
 
-    assert_equal 0, @st_time.hours
-    assert_equal 2, @st_time.minutes
-    assert_equal 10, @st_time.seconds
-    assert_equal secs, @st_time.to_i
+    assert_equal 0, @dl_time.hours
+    assert_equal 2, @dl_time.minutes
+    assert_equal 10, @dl_time.seconds
+    assert_equal secs, @dl_time.to_i
   end
 
 
   def test_create_with_zero_seconds
-    @st_time = DatelessTime.new 0
+    @dl_time = DatelessTime.new 0
 
-    assert_equal 0, @st_time.hours
-    assert_equal 0, @st_time.minutes
-    assert_equal 0, @st_time.seconds
-    assert_equal 0, @st_time.to_i
+    assert_equal 0, @dl_time.hours
+    assert_equal 0, @dl_time.minutes
+    assert_equal 0, @dl_time.seconds
+    assert_equal 0, @dl_time.to_i
   end
 
 
   def test_create_with_too_many_seconds
     assert_raises DatelessTime::TimeOutOfRangeError do
-      @st_time = DatelessTime.new(DatelessTime::SECONDS_IN_24_HOURS + 1)
+      @dl_time = DatelessTime.new(DatelessTime::SECONDS_IN_24_HOURS + 1)
     end
   end
 
 
 
   def test_create_with_hash
-    @st_time = DatelessTime.new({ hours: 1, minutes: 30, seconds: 45 })
+    @dl_time = DatelessTime.new({ hours: 1, minutes: 30, seconds: 45 })
 
-    assert_equal 1, @st_time.hours
-    assert_equal 30, @st_time.minutes
-    assert_equal 45, @st_time.seconds
-    assert_equal (45 + (30 * 60) + (1 * 60 * 60)), @st_time.to_i
+    assert_equal 1, @dl_time.hours
+    assert_equal 30, @dl_time.minutes
+    assert_equal 45, @dl_time.seconds
+    assert_equal (45 + (30 * 60) + (1 * 60 * 60)), @dl_time.to_i
   end
 
 
   def test_create_with_hash_with_extra_keys
-    @st_time = DatelessTime.new({ hours: 1, minutes: 30, seconds: 45, foo: "bar" })
+    @dl_time = DatelessTime.new({ hours: 1, minutes: 30, seconds: 45, foo: "bar" })
 
-    assert_equal 1, @st_time.hours
-    assert_equal 30, @st_time.minutes
-    assert_equal 45, @st_time.seconds
-    assert_equal (45 + (30 * 60) + (1 * 60 * 60)), @st_time.to_i
+    assert_equal 1, @dl_time.hours
+    assert_equal 30, @dl_time.minutes
+    assert_equal 45, @dl_time.seconds
+    assert_equal (45 + (30 * 60) + (1 * 60 * 60)), @dl_time.to_i
   end
 
 
   def test_create_with_hash_without_seconds
-    @st_time = DatelessTime.new({ hours: 1, minutes: 30 })
+    @dl_time = DatelessTime.new({ hours: 1, minutes: 30 })
 
-    assert_equal 1, @st_time.hours
-    assert_equal 30, @st_time.minutes
-    assert_equal 0, @st_time.seconds
-    assert_equal ((30 * 60) + (1 * 60 * 60)), @st_time.to_i
+    assert_equal 1, @dl_time.hours
+    assert_equal 30, @dl_time.minutes
+    assert_equal 0, @dl_time.seconds
+    assert_equal ((30 * 60) + (1 * 60 * 60)), @dl_time.to_i
   end
 
 
   def test_create_with_hash_without_hours
     assert_raises DatelessTime::InitializationError do
-      @st_time = DatelessTime.new({ minutes: 30 })
+      @dl_time = DatelessTime.new({ minutes: 30 })
     end
   end
 
 
   def test_create_with_empty_hash
     assert_raises DatelessTime::InitializationError do
-      @st_time = DatelessTime.new({})
+      @dl_time = DatelessTime.new({})
     end
   end
 
 
   def test_create_with_hash_without_hours_but_other_random_keys
     assert_raises DatelessTime::InitializationError do
-      @st_time = DatelessTime.new({ batman: "nananana" })
+      @dl_time = DatelessTime.new({ batman: "nananana" })
     end
   end
 
 
   def test_create_with_hash_without_seconds_and_minutes
-    @st_time = DatelessTime.new({ hours: 1 })
+    @dl_time = DatelessTime.new({ hours: 1 })
 
-    assert_equal 1, @st_time.hours
-    assert_equal 0, @st_time.minutes
-    assert_equal 0, @st_time.seconds
-    assert_equal (1 * 60 * 60), @st_time.to_i
+    assert_equal 1, @dl_time.hours
+    assert_equal 0, @dl_time.minutes
+    assert_equal 0, @dl_time.seconds
+    assert_equal (1 * 60 * 60), @dl_time.to_i
   end
 
 
   def test_create_with_array
-    @st_time = DatelessTime.new [2, 11, 44]
+    @dl_time = DatelessTime.new [2, 11, 44]
 
-    assert_equal 2, @st_time.hours
-    assert_equal 11, @st_time.minutes
-    assert_equal 44, @st_time.seconds
-    assert_equal (44 + (11 * 60) + (2 * 60 * 60)), @st_time.to_i
+    assert_equal 2, @dl_time.hours
+    assert_equal 11, @dl_time.minutes
+    assert_equal 44, @dl_time.seconds
+    assert_equal (44 + (11 * 60) + (2 * 60 * 60)), @dl_time.to_i
   end
 
 
   def test_create_with_array_with_extra_elements
-    @st_time = DatelessTime.new [2, 11, 44, 1, 2, 3]
+    @dl_time = DatelessTime.new [2, 11, 44, 1, 2, 3]
 
-    assert_equal 2, @st_time.hours
-    assert_equal 11, @st_time.minutes
-    assert_equal 44, @st_time.seconds
-    assert_equal (44 + (11 * 60) + (2 * 60 * 60)), @st_time.to_i
+    assert_equal 2, @dl_time.hours
+    assert_equal 11, @dl_time.minutes
+    assert_equal 44, @dl_time.seconds
+    assert_equal (44 + (11 * 60) + (2 * 60 * 60)), @dl_time.to_i
   end
 
 
   def test_create_with_array_without_seconds
-    @st_time = DatelessTime.new [2, 11]
+    @dl_time = DatelessTime.new [2, 11]
 
-    assert_equal 2, @st_time.hours
-    assert_equal 11, @st_time.minutes
-    assert_equal 0, @st_time.seconds
-    assert_equal ((11 * 60) + (2 * 60 * 60)), @st_time.to_i
+    assert_equal 2, @dl_time.hours
+    assert_equal 11, @dl_time.minutes
+    assert_equal 0, @dl_time.seconds
+    assert_equal ((11 * 60) + (2 * 60 * 60)), @dl_time.to_i
   end
 
 
   def test_create_with_array_without_seconds_and_minutes
-    @st_time = DatelessTime.new [2]
+    @dl_time = DatelessTime.new [2]
 
-    assert_equal 2, @st_time.hours
-    assert_equal 0, @st_time.minutes
-    assert_equal 0, @st_time.seconds
-    assert_equal (2 * 60 * 60), @st_time.to_i
+    assert_equal 2, @dl_time.hours
+    assert_equal 0, @dl_time.minutes
+    assert_equal 0, @dl_time.seconds
+    assert_equal (2 * 60 * 60), @dl_time.to_i
   end
 
 
   def test_create_with_empty_array
     assert_raises DatelessTime::InitializationError do
-      @st_time = DatelessTime.new([])
+      @dl_time = DatelessTime.new([])
     end
   end
 
 
   def test_create_with_array_with_bad_data
     assert_raises DatelessTime::TimeOutOfRangeError do
-      @st_time = DatelessTime.new([25, 10, 11])
+      @dl_time = DatelessTime.new([25, 10, 11])
     end
 
     assert_raises DatelessTime::TimeOutOfRangeError do
-      @st_time = DatelessTime.new([11, 67, 11])
+      @dl_time = DatelessTime.new([11, 67, 11])
     end
 
     assert_raises DatelessTime::TimeOutOfRangeError do
-      @st_time = DatelessTime.new([9, 10, 100])
+      @dl_time = DatelessTime.new([9, 10, 100])
     end
   end
 
@@ -293,31 +293,31 @@ class CreationTest < Minitest::Test
 
   def test_create_without_argument
     @time = Time.now
-    @st_time = DatelessTime.new
+    @dl_time = DatelessTime.new
 
-    assert_equal @time.hour, @st_time.hours
-    assert_equal @time.min, @st_time.minutes
-    assert_equal @time.sec, @st_time.seconds
-    assert_equal @time.strftime("%H:%M:%S"), @st_time.to_s
-    assert_equal (@time.sec + (@time.min * 60) + (@time.hour * 60 * 60)), @st_time.to_i
+    assert_equal @time.hour, @dl_time.hours
+    assert_equal @time.min, @dl_time.minutes
+    assert_equal @time.sec, @dl_time.seconds
+    assert_equal @time.strftime("%H:%M:%S"), @dl_time.to_s
+    assert_equal (@time.sec + (@time.min * 60) + (@time.hour * 60 * 60)), @dl_time.to_i
   end
 
 
 
   def test_create_with_nil
     assert_raises DatelessTime::InitializationError do
-      @st_time = DatelessTime.new nil
+      @dl_time = DatelessTime.new nil
     end
   end
 
 
   def test_create_with_unsupported_objects
     assert_raises DatelessTime::InitializationError do
-      @st_time = DatelessTime.new (1..4)
+      @dl_time = DatelessTime.new (1..4)
     end
 
     assert_raises DatelessTime::InitializationError do
-      @st_time = DatelessTime.new 1.21
+      @dl_time = DatelessTime.new 1.21
     end
   end
 end
--- test/query_test.rb
+++ test/query_test.rb
@@ -3,13 +3,13 @@ require 'test_helper'
 class QueryTest < Minitest::Test
 
   def setup
-    @st_time = DatelessTime.new [13, 37, 42]
+    @dl_time = DatelessTime.new [13, 37, 42]
   end
 
 
 
   def test_to_time_without_base
-    @to_time = @st_time.to_time
+    @to_time = @dl_time.to_time
     now = Time.now
     expected = Time.new(now.year, now.month, now.day, 13, 37, 42, now.utc_offset)
 
@@ -20,7 +20,7 @@ class QueryTest < Minitest::Test
 
   def test_to_time_with_base
     base = Time.new(1990, 11, 10, 12, 13, 14)
-    @to_time = @st_time.to_time(base)
+    @to_time = @dl_time.to_time(base)
     
     assert_equal Time, @to_time.class
     assert_equal Time.new(1990, 11, 10, 13, 37, 42), @to_time
@@ -28,62 +28,62 @@ class QueryTest < Minitest::Test
 
 
   def test_to_s
-    assert_equal "13:37:42", @st_time.to_s
+    assert_equal "13:37:42", @dl_time.to_s
   end
 
 
   def test_to_s_with_padding_zeroes
-    @st_time = DatelessTime.new [2, 7, 9]
-    assert_equal "02:07:09", @st_time.to_s
+    @dl_time = DatelessTime.new [2, 7, 9]
+    assert_equal "02:07:09", @dl_time.to_s
   end
 
 
   def test_to_h
-    assert_equal({ hours: 13, minutes: 37, seconds: 42 }, @st_time.to_h)
+    assert_equal({ hours: 13, minutes: 37, seconds: 42 }, @dl_time.to_h)
   end
 
 
   def test_to_a
-    assert_equal [13, 37, 42], @st_time.to_a
+    assert_equal [13, 37, 42], @dl_time.to_a
   end
 
 
   def test_seconds_since_midnight
     secs = 42 + (37 * 60) + (13 * 60 * 60)
-    assert_equal secs, @st_time.seconds_since_midnight
+    assert_equal secs, @dl_time.seconds_since_midnight
   end
 
 
   def test_to_i
     secs = 42 + (37 * 60) + (13 * 60 * 60)
-    assert_equal secs, @st_time.to_i
+    assert_equal secs, @dl_time.to_i
   end
 
 
   def test_accessor_hours
-    assert @st_time.respond_to?(:hours)
-    assert_equal 13, @st_time.hours
+    assert @dl_time.respond_to?(:hours)
+    assert_equal 13, @dl_time.hours
   end
 
 
   def test_accessor_minutes
-    assert @st_time.respond_to?(:minutes)
-    assert_equal 37, @st_time.minutes
+    assert @dl_time.respond_to?(:minutes)
+    assert_equal 37, @dl_time.minutes
   end
 
 
   def test_accessor_seconds
-    assert @st_time.respond_to?(:seconds)
-    assert_equal 42, @st_time.seconds
+    assert @dl_time.respond_to?(:seconds)
+    assert_equal 42, @dl_time.seconds
   end
 
 
   def test_strftime
-    assert_equal "13:37", @st_time.strftime("%H:%M")
-    assert_equal "13:37:42", @st_time.strftime("%H:%M:%S")
-    assert_equal "13-37-42", @st_time.strftime("%H-%M-%S")
+    assert_equal "13:37", @dl_time.strftime("%H:%M")
+    assert_equal "13:37:42", @dl_time.strftime("%H:%M:%S")
+    assert_equal "13-37-42", @dl_time.strftime("%H-%M-%S")
 
-    assert_equal "1:37 pm", @st_time.strftime("%-l:%M %P")
+    assert_equal "1:37 pm", @dl_time.strftime("%-l:%M %P")
   end
 
 
